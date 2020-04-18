0e8485ac4af4a43aa0c6b0302075a3987e07cb64
Jake Caban-Tomski <jacque006@gmail.com>
2020-04-17 23:29:39 -0600

    Add the larch

--- source/states/MovementState.hx
+++ source/states/MovementState.hx
@@ -1,12 +1,19 @@
 package states;
 
+import flixel.util.FlxCollision;
+import flixel.FlxSprite;
+import flixel.group.FlxGroup;
 import entities.Player;
+import entities.Tree;
 import flixel.FlxG;
 import flixel.FlxState;
 
 class MovementState extends FlxState
 {
 	var player:Player;
+	var playerGroup:FlxGroup;
+
+	var treeGroup:FlxGroup;
 
 	override public function create():Void
 	{
@@ -14,12 +21,29 @@ class MovementState extends FlxState
 		FlxG.debugger.visible = true;
 		FlxG.debugger.drawDebug = true;
 		
+		playerGroup = new FlxGroup(1);
+		add(playerGroup);
+
 		player = new Player();
-		add(player);
+		playerGroup.add(player);
+
+		treeGroup = new FlxGroup(100);
+		add(treeGroup);
+
+		var tree = new Tree();
+		tree.x = player.x + 100;
+		tree.y = player.y + 100;
+		treeGroup.add(tree);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+
+		FlxG.collide(playerGroup, treeGroup);
+	}
+
+	private static function pixelPerfect(a: FlxSprite, b: FlxSprite): Bool {
+		return FlxCollision.pixelPerfectCheck(a, b, 1);
 	}
 }
