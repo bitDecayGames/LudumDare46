ea9213aa280d1469f58bb7adf1dae4fbd99812cc
Tristan Havelick <tristan@havelick.com>
2020-04-19 13:10:15 -0600

    Add funky fire sprite

--- source/entities/Fire.hx
+++ source/entities/Fire.hx
@@ -4,10 +4,13 @@ import flixel.FlxG;
 import flixel.effects.particles.FlxEmitter;
 import flixel.util.FlxColor;
 import flixel.group.FlxGroup;
+import entities.FireArt;
+
 
 class Fire extends FlxGroup
 {
     public var emitter:FlxEmitter;
+    public var fireArt:FireArt;
     public var dead:Bool;
     public var duration:Float;
     public var MAX_DURATION:Float = 30;
@@ -27,16 +30,19 @@ class Fire extends FlxGroup
         }
 
         this.duration = duration;
+        fireArt = new FireArt(x, y);
+        add(fireArt);
         
-        emitter = new FlxEmitter(x, y, 200);
+        emitter = new FlxEmitter(x + fireArt.width / 2 - 13, y + fireArt.height / 2 - 23, 200);
 		emitter.makeParticles(4, 6, FlxColor.ORANGE, 200);
 		emitter.color.set(FlxColor.YELLOW, FlxColor.RED, FlxColor.BLACK);
         emitter.launchAngle.set(-80, -100);
-        emitter.scale.set(0.5, 1, 2, 2.5);
-        emitter.alpha.set(1,1,0,0);
-    
-        add(emitter);
+        emitter.scale.set(0.25, 0.5, 1, 1.25);
+        emitter.alpha.set(0.5, 0.5, 0, 0);
         resize();
+
+        add(emitter);
+    
     }
 
     override public function update(elapsed:Float):Void
@@ -52,6 +58,7 @@ class Fire extends FlxGroup
             dead = true;
             emitter.frequency = 5000;   // this number goes big so we can essentially stop this emiter, 
                                         // but the existing particles won't just disappear
+            fireArt.kill();
         }
         else {
             emitter.frequency = getFrequency();
@@ -73,8 +80,25 @@ class Fire extends FlxGroup
             addTime(-5);
         }
     
+        updateAnimation(duration);
     }
     
+
+    private function updateAnimation(duration):Void
+    {
+        trace(duration);
+        var newAnimation:String;
+        if (duration > 2 / 3 * MAX_DURATION) {
+            newAnimation = "raging";
+        } else if (duration > 1 / 3 * MAX_DURATION) {
+            newAnimation = "regular";
+        } else {
+            newAnimation = "tiny";
+        }
+
+        fireArt.switchAnimation(newAnimation);
+    }
+
     private function getFrequency():Float
     {
         return deathRate * duration + MAX_FREQUENCY;
