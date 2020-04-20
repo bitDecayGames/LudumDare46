824bfa7005aa6b1082950becaf021b3333efd52c
Jake Caban-Tomski <jacque006@gmail.com>
2020-04-20 11:06:09 -0600

    Move z getter to function

--- source/sorting/HitboxSorter.hx
+++ source/sorting/HitboxSorter.hx
@@ -5,14 +5,19 @@ import flixel.util.FlxSort;
 import entities.WaterSplash;
 
 class HitboxSorter {
+	static var ARIBITRARY_LARGE_VALUE = 9999999;
+
+	private static function getSpriteZ(s: FlxSprite): Int {
+		return Type.getClass(s) == WaterSplash ? ARIBITRARY_LARGE_VALUE : 0;
+	}
+
 	// sort will sort things based on the CENTER-Y of their hitboxes
 	public static inline function sort(order:Int, s1:FlxSprite, s2:FlxSprite):Int {
-		// HACKS figure out another way to do this
-		var largeValue = 999999;
-		var s1Z = Type.getClass(s1) == WaterSplash ? largeValue : 0;
-		var s2Z = Type.getClass(s2) == WaterSplash ? largeValue : 0;
-
-		return FlxSort.byValues(order, s1.y + (s1.height / 2) + s1Z, s2.y + (s2.height / 2) + s2Z);
+		return FlxSort.byValues(
+			order,
+			s1.y + (s1.height / 2) + getSpriteZ(s1),
+			s2.y + (s2.height / 2) + getSpriteZ(s2)
+		);
 	}
 
 	public static inline function sortNested(order:Int, s1:FlxSprite, s2:FlxSprite):Int {
