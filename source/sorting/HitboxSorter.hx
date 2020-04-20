997c087c73b53e32fe9664821002a1f94c1b4319
Jake Caban-Tomski <jacque006@gmail.com>
2020-04-20 00:28:26 -0600

    Use reflection instead of casting

--- source/sorting/HitboxSorter.hx
+++ source/sorting/HitboxSorter.hx
@@ -9,22 +9,8 @@ class HitboxSorter {
 	public static inline function sort(order:Int, s1:FlxSprite, s2:FlxSprite):Int {
 		// HACKS figure out another way to do this
 		var largeValue = 999999;
-		var s1Z = 0;
-		var s2Z = 0;
-
-		try {
-			var s1Water = cast(s1, WaterSplash);
-			s1Z = largeValue;
-		} catch(err:Any) {
-			// no-op
-		}
-
-		try {
-			var s2Water = cast(s2, WaterSplash);
-			s2Z = largeValue;
-		} catch(err:Any) {
-			// no-op
-		}
+		var s1Z = Type.getClass(s1) == WaterSplash ? largeValue : 0;
+		var s2Z = Type.getClass(s2) == WaterSplash ? largeValue : 0;
 
 		return FlxSort.byValues(order, s1.y + (s1.height / 2) + s1Z, s2.y + (s2.height / 2) + s2Z);
 	}
